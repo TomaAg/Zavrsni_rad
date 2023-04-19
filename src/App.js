be13daa59baf584820665039bac24dfc4c6664a4
toma.a <toma.aglic1@gmail.com>
2023-04-19 16:15:04 +0200

    Deployed website

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/Zavrsni_rad" element={<HomePage />} />
         <Route path="/indexPage" element={<IndexPage />} />
         <Route path="/studentJobPage" element={<StudentJobPage />} />
       </Routes>
